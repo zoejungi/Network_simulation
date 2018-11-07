c690dfbb5247bdca07975c90100da42d23a477bc
zoejungi <44709507+zoejungi@users.noreply.github.com>
2018-11-07 15:36:53 +0100

    Update network.h

--- network.h
+++ network.h
@@ -12,22 +12,28 @@ class Network {
 public:
     Network();
 
-/*! Resizes the list of nodes (\ref values) */
+/*! Resizes the list of nodes (\ref values) 
+After this function is called \ref values has size *n* and contains random numbers (normal distribution, mean=0, sd=1).
+*/
     void resize(const size_t& new_size);
 /*! Adds a bidirectional link between two nodes
   @param[in] a,b the indexes if the two nodes 
-  @param[out] success the link was succesfully inserted
+  @param[out] success the link was succesfully inserted (true if both nodes exist and the link did not exist yet)
  */
     bool add_link(const size_t& a, const size_t& b);
-/*! Creates random connections between nodes: each node *n* will be linked with *degree(n)* other nodes (randomly chosen) where *degree(n)* is Poisson-distributed.
-
-  All previous links are cleared first.
-  @param mean_deg the average of the Poisson distribution.
+/*! Creates random connections between nodes: each node *n* will be linked with *degree(n)* other nodes (randomly chosen),
+  where *degree(n)* is Poisson-distributed.
+  All previous links are erased first.
+  @param[in] mean_deg the average of the Poisson distribution,
+  @param[out] number of links created.
  */
     size_t random_connect(const double& mean_deg);
-/*! Resets all node values.
-  @param[in] vector of new node values
-  @param[out] number of nodes succesfully reset
+/*! Resets node values with a vector of *n* new values.
+  If the current size is *s* and *n<s* then only the first *n* values are changed.
+  If *n>s* then only *s* values are used. 
+  The internal \ref values vector keeps the same size.
+  @param[in] vector of new node values,
+  @param[out] number of nodes succesfully reset.
  */
     size_t set_values(const std::vector<double>& new_values);
 /*! Number of nodes */
@@ -36,7 +42,9 @@ public:
     size_t degree(const size_t& _n) const;
 /*! Value of node no *n* */
     double value(const size_t& _n) const;
-/*! All node values in descending order */
+/*! All node values in descending order.
+    \ref values is left unchanged by this operation.
+ */
     std::vector<double> sorted_values() const;
 /*! All neighbors (linked) nodes of node no *n* */
     std::vector<size_t> neighbors(const size_t& _n) const;
